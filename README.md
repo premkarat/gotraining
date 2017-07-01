eb23dd682f67e966a1919bec9e3400b4382728a9
Prem Karat <prem.karat@gmail.com>
2017-07-02 00:02:45 +0530

    introducing-go: chapter 1 solutions
    Signed-off-by: Prem Karat <prem.karat@gmail.com>

---
+++ README.md
@@ -0,0 +1,3 @@
+###Go Training
+Exercises and samples from following learning resources
+1. [introducing-go](http://www.golang-book.com/books/intro)
---
+++ introducing-go/chapter2/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("1.0 + 1.0 = ", 1.0+1.0)
+	fmt.Println(len("Hello, World"))
+	fmt.Println("Hello, World"[1])
+	fmt.Println("Hello, " + "World")
+	fmt.Println(true && true)
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(true || true)
+	fmt.Println(!true)
+}
